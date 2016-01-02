0736158aa31d480abe51dd655c00bdfd05104125
Tunc <Tunc@Tunc-MacBook-Pro.local>
2016-01-02 16:05:34 +0200

    token deleted

--- incomingFaxes/inc/config.php
+++ incomingFaxes/inc/config.php
@@ -6,4 +6,4 @@
  * Time: 23:51
  */
 
-$token      = "4aea664103ab49f68c1f1a970abc2d42c18c072826ad61356befa0913eec9c8d"; // Bulutfon panelinden aldığımız master token
+$token      = ""; // Bulutfon panelinden aldığımız master token
